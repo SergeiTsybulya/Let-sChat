f8ecd9d41ee639568409cad069b100b6c4e1a2f0
SergeiTsybulya <sergtsybulya@gmail.com>
2022-08-29 21:10:48 +0300

    Authentication enabled

--- Chat/Controllers/RegisterViewController.swift
+++ Chat/Controllers/RegisterViewController.swift
@@ -7,13 +7,27 @@
 //
 
 import UIKit
+import Firebase
 
 class RegisterViewController: UIViewController {
-
+    
     @IBOutlet weak private var emailTextfield: UITextField!
     @IBOutlet weak private var passwordTextfield: UITextField!
     
     @IBAction private func registerPressed(_ sender: UIButton) {
+        
+        if let email = emailTextfield.text, let password = passwordTextfield.text {
+            
+            Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+                if let e = error {
+                    let alert = UIAlertController(title: "Error", message: "\(e.localizedDescription)", preferredStyle: UIAlertController.Style.alert)
+                    alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+                    self.present(alert, animated: true, completion: nil)
+                } else {
+                    self.performSegue(withIdentifier: "RegisterToChat", sender: self )
+                }
+            }
+        }
     }
-    
 }
+
