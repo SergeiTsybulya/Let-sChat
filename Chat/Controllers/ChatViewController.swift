ea70de3a31633655e167cfb0abe2ca4545c976dd
SergeiTsybulya <sergtsybulya@gmail.com>
2022-08-30 21:19:26 +0300

    LogOut enabled

--- Chat/Controllers/ChatViewController.swift
+++ Chat/Controllers/ChatViewController.swift
@@ -7,19 +7,32 @@
 //
 
 import UIKit
+import Firebase
 
 class ChatViewController: UIViewController {
-
+    
     @IBOutlet weak private var tableView: UITableView!
     @IBOutlet weak private var messageTextfield: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        title = "⚡️ChatChat⚡️"
+        navigationItem.hidesBackButton = true
     }
     
     @IBAction private func sendPressed(_ sender: UIButton) {
     }
     
-
+    @IBAction func logOutPressed(_ sender: UIBarButtonItem) {
+        let firebaseAuth = Auth.auth()
+        do {
+            try firebaseAuth.signOut()
+            navigationController?.popToRootViewController(animated: true)
+            
+        } catch let signOutError as NSError {
+            print ("Error signing out : $@", signOutError)
+        }
+        
+    }
+    
 }
